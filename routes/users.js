d6abc10546348016e5ce88d92d9583fdbc132059
Nadhem <oueslati.nadhem@gmail.com>
2024-09-23 12:09:34 +0200

    second commit

--- routes/users.js
+++ routes/users.js
@@ -4,22 +4,22 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.status(200).json({message: 'User list'});
+  res.status(200).json({ message: 'User list' });
 });
 /* GET users listing. */
 router.get('/:id', (req, res) => {
-  res.status(200).json({message: 'User id'});
+  res.status(200).json({ message: 'User id' });
 });
 /* GET users listing. */
 router.post('/', (req, res) => {
-  res.status('r').json({message: 'User id created successfully'});
+  res.status('r').json({ message: 'User id created successfully' });
 });
 /* GET users listing. */
 router.put('/:id', (req, res) => {
-  res.status(200).json({message: 'User id modified successfully'});
+  res.status(200).json({ message: 'User id modified successfully' });
 });
 /* GET users listing. */
 router.delete('/:id', (req, res) => {
-  res.status(200).json({message: 'User id deleted successfully'});
+  res.status(200).json({ message: 'User id deleted successfully' });
 });
 module.exports = router;
